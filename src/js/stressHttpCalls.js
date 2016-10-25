afb2f362fa4620b8a2e492870589079333eb8f9d
Ryan Chen <me@ryanchen.com>
2016-10-25 15:03:10 -0700

    Update stress call

--- src/js/stressHttpCalls.js
+++ src/js/stressHttpCalls.js
@@ -9,18 +9,22 @@ if (process.argv.length <3) {
   const host = process.argv[2] || 'www.google.com';
   const path = process.argv[3] || '/';
   const keyword = process.argv[4] || 'a';
-  const SAMPLE_SIZE = 10;
+  const SAMPLE_SIZE = 100;
 
   let httpCount = 0;
   let resultCount = 0;
 
   for (let i=1; i<=SAMPLE_SIZE; i++) {
+    const finalPath = path + Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    //console.log(host);
+    //console.log(finalPath);
     https.get({
       hostname: host,
-      path: path + Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+      path: finalPath
     }, response => {
       response.setEncoding('utf8');
       response.on('data', d => {
+        //console.log(d);
         if (d.indexOf(keyword) > -1) {
           resultCount = resultCount + 1;
         }
